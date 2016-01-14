6bb11fd2172b549914eb11a1880c4236bbec275a
Sawyer Vaughan <sawyer.vaughan@students.olin.edu>
2016-01-13 18:28:58 -0700

    Update for theos

--- minimalhud/MinimalHUD.mm
+++ minimalhud/MinimalHUD.mm
@@ -7,26 +7,27 @@
 @implementation MinimalHUDListController
 - (id)specifiers {
 	if(_specifiers == nil) {
-		_specifiers = [[self loadSpecifiersFromPlistName:@"MinimalHUD" target:self] retain];
-	}
+		NSMutableArray *specs = [NSMutableArray init];
+		[specs addObjectsFromArray: [self loadSpecifiersFromPlistName:@"MinimalHUD" target:self]];
 	
-	PSSpecifier* specifier = [PSSpecifier preferenceSpecifierNamed:@"title"
-                                                        target:self
-                                                           set:NULL
-                                                           get:NULL
-                                                        detail:NSClassFromString(@"PSListItemsController")
-                                                          cell:PSLinkListCell
-                                                          edit:Nil];
- 
-	[specifier setProperty:@YES forKey:@"enabled"];
-	[specifier setProperty:@"0" forKey:@"default"];		
-	specifier.values = [NSArray arrayWithObjects:@"0",@"1",@"2",nil];
-	specifier.titleDictionary = [NSDictionary dictionaryWithObjects:[NSArray arrayWithObjects:@"Title 1",@"Title 2",@"Title 3",nil] forKeys:specifier.values];
-	specifier.shortTitleDictionary = [NSDictionary dictionaryWithObjects:[NSArray arrayWithObjects:@"T1",@"T@",@"T3",nil] forKeys:specifier.values];
-	[specifier setProperty:@"kListValue" forKey:@"key"];
+		PSSpecifier* specifier = [PSSpecifier preferenceSpecifierNamed:@"title"
+	                                                        target:self
+	                                                           set:NULL
+	                                                           get:NULL
+	                                                        detail:NSClassFromString(@"PSListItemsController")
+	                                                          cell:PSLinkListCell
+	                                                          edit:Nil];
 	 
-	// Now add the specifier to your controller.
-	[_specifiers append:specifier];
+		[specifier setProperty:@YES forKey:@"enabled"];
+		[specifier setProperty:@"0" forKey:@"default"];		
+		specifier.values = [NSArray arrayWithObjects:@"0",@"1",@"2",nil];
+		specifier.titleDictionary = [NSDictionary dictionaryWithObjects:[NSArray arrayWithObjects:@"Title 1",@"Title 2",@"Title 3",nil] forKeys:specifier.values];
+		specifier.shortTitleDictionary = [NSDictionary dictionaryWithObjects:[NSArray arrayWithObjects:@"T1",@"T@",@"T3",nil] forKeys:specifier.values];
+		[specifier setProperty:@"kListValue" forKey:@"key"];
+		 
+		// Now add the specifier to your controller.
+		[specs addObject:specifier];
+	}
 
 	return _specifiers;
 }
