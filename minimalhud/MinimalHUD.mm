7fd97033c04516e656fbad8eeb89351207d8de95
Sawyer Vaughan <svaughan@onshape.com>
2016-08-17 19:41:09 -0500

    Preferences might work

--- minimalhud/MinimalHUD.mm
+++ minimalhud/MinimalHUD.mm
@@ -21,9 +21,21 @@
 	return _specifiers;
 }
 
-- (void)setValue:(id)value forSpecifier:(id)specifier
-{
-	NSLog(@"%@ %@", value, specifier);
+- (id)readPreferenceValue:(PSSpecifier*)specifier {
+	NSString *path = [NSString stringWithFormat:@"/var/mobile/Library/Preferences/%@.plist", specifier.properties[@"defaults"]];
+	NSDictionary *settings = [NSDictionary dictionaryWithContentsOfFile:path];
+	return (settings[specifier.properties[@"key"]]) ?: specifier.properties[@"default"];
+}
+
+- (void)setPreferenceValue:(id)value specifier:(PSSpecifier*)specifier {
+	NSString *path = [NSString stringWithFormat:@"/var/mobile/Library/Preferences/%@.plist", specifier.properties[@"defaults"]];
+	NSMutableDictionary *settings = [NSMutableDictionary dictionaryWithContentsOfFile:path]];
+	[settings setObject:value forKey:specifier.properties[@"key"]];
+	[settings writeToFile:path atomically:YES];
+	CFStringRef notificationName = (CFStringRef)specifier.properties[@"PostNotification"];
+	if (notificationName) {
+		CFNotificationCenterPostNotification(CFNotificationCenterGetDarwinNotifyCenter(), notificationName, NULL, NULL, YES);
+	}
 }
 
 @end
