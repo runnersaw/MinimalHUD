8ad3e3e70d26790c0050e140bb577d6b3f1c7155
Sawyer Vaughan <svaughan@onshape.com>
2016-08-17 19:26:29 -0500

    Preferences might work

--- minimalhud/MinimalHUD.mm
+++ minimalhud/MinimalHUD.mm
@@ -8,13 +8,22 @@
 - (id)specifiers {
 	if(_specifiers == nil) {
 		NSMutableArray *specs = [[NSMutableArray alloc] init];
+
 		[specs addObjectsFromArray: [[self loadSpecifiersFromPlistName:@"MinimalHUD" target:self] retain]];
 		[specs addObjectsFromArray: [[self loadSpecifiersFromPlistName:@"color_mode" target:self] retain]];
+		[specs addObjectsFromArray: [[self loadSpecifiersFromPlistName:@"color_mode_theme" target:self] retain]];
+		[specs addObjectsFromArray: [[self loadSpecifiersFromPlistName:@"location_mode" target:self] retain]];
+		[specs addObjectsFromArray: [[self loadSpecifiersFromPlistName:@"location_mode_preset" target:self] retain]];
 
 		_specifiers = specs;
 	}
 
 	return _specifiers;
+}
+
+- (void)setValue:(id)value forSpecifier:(id)specifier
+{
+	
 }
 
 @end
