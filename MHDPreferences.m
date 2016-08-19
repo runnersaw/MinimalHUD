4a8a47d06826644dbc6f16467ba05327ac09a7d9
Sawyer Vaughan <svaughan@onshape.com>
2016-08-19 17:06:27 -0500

    Hopefully working preferences

---
+++ MHDPreferences.m
@@ -0,0 +1,72 @@
+#import "MHDPreferences.h"
+
+@interface MHDPreferences ()
+
+@property (nonatomic, readwrite) BOOL enabled;
+@property (nonatomic, readwrite) MHDColorMode colorMode;
+@property (nonatomic, readwrite) MHDColorTheme colorTheme;
+@property (nonatomic, strong) UIColor *startingColor;
+@property (nonatomic, strong) UIColor *endingColor;
+@property (nonatomic, strong) UIColor *backgroundColor;
+@property (nonatomic, readwrite) MHDLocationMode locationMode;
+@property (nonatomic, readwrite) MHDLocationPreset locationPreset;
+@property (nonatomic, readwrite) MHDLocationOrientation locationOrientation;
+@property (nonatomic, readwrite) CGFloat locationX;
+@property (nonatomic, readwrite) CGFloat locationY;
+
+@end
+
+@implementation MHDPreferences
+
+- (instancetype)initWithSettings:(NSDictionary *)settings
+{
+	self = [super init];
+	if (self)
+	{
+		NSNumber *e = settings[@"enabled"];
+		self.enabled = e ? e.boolValue : NO;
+
+		NSNumber *cM = settings[@"colorMode"];
+		self.colorMode = cM ? cM.unsignedIntegerValue : MHDColorModeTheme;
+
+		if (self.colorMode == MHDColorModeTheme)
+		{
+			NSNumber *cT = settings[@"colorTheme"];
+			self.colorTheme = cT ? cT.unsignedIntegerValue : MHDColorThemeWarm;
+		}
+		else if (self.colorMode == MHDColorModeCustom)
+		{
+			NSString *sC = settings[@"startingColor"];
+			self.startingColor = sC ? colorFromString(sC) : [UIColor whiteColor];
+
+			NSString *eC = settings[@"endingColor"];
+			self.endingColor = eC ? colorFromString(eC) : [UIColor whiteColor];
+
+			NSString *bC = settings[@"backgroundColor"];
+			self.backgroundColor = bC ? colorFromString(bC) : [UIColor blackColor];
+		}
+
+		NSNumber *lM = settings[@"locationMode"];
+		self.locationMode = lM.unsignedIntegerValue;
+
+		if (self.locationMode == MHDLocationModePreset)
+		{
+			NSNumber *lP = settings[@"locationPreset"];
+			self.locationPreset = lP ? lP.unsignedIntegerValue : MHDLocationPresetTop;
+		}
+		else if (self.locationMode == MHDLocationModeCustom)
+		{
+			NSNumber *lO = settings[@"locationOrientation"];
+			self.locationOrientation = lO ? lO.unsignedIntegerValue : MHDLocationOrientationHorizontal;
+
+			NSString *lX = settings[@"locationX"];
+			self.locationX = lX ? cgFloatFromString(lX) : 0.0;
+
+			NSString *lY = settings[@"locationY"];
+			self.locationY = lY ? cgFloatFromString(lY) : 0.0;
+		}
+	}
+	return self;
+}
+
+@end
